805bc3e09bf0fb98b62ab83e7dcbc6643fdc5ec8
Bonnie Jordan <thebonstergirl@gmail.com>
2018-08-28 21:01:57 -0500

    finished

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const checkForSession = require('./middlewares/checkForSession');
 const {read} = require('./controllers/swag_controller');
 const {login, register, signout, getUser} = require('./controllers/auth_controller');
+const cart_controller = require('./controllers/cart_controller');
+const search_controller = require('./search_controller');
 
 const app = express();
 
@@ -16,12 +18,17 @@ app.use(session({
 }));
 
 app.use(checkForSession);
+app.use(express.static(`${__dirname}/build`));
 
 app.get('/api/swag', read);
 app.get('/api/user', getUser);
 app.post('/api/login', login);
 app.post('/api/register', register);
 app.post('/api/signout', signout);
+app.post('/api/cart', cart_controller.add);
+app.post('/api/cart/checkout', cart_controller.checkout);
+app.delete('/api/cart', cart_controller.delete);
+app.get('/api/search', search_controller.search);
 
 const port = process.env.SERVER_PORT || 3000;
 app.listen(port, ()=> {console.log(`Sever listening on port: ${port} ;D`)});
